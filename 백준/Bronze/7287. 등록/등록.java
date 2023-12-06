04deffc230ff023ec81399f47a57b337ba700113
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-06 23:26:58 +0900

    [Bronze V] Title: 등록, Time: 108 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(2);
+        System.out.println("1104minho");
+
+    }
+}
+
