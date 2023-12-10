882caacaefde1ca6a8ebcf4986fdf71c50335c36
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-10 20:02:53 +0900

    [Bronze V] Title: 문자와 문자열, Time: 216 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int i = sc.nextInt();
+
+        s.split("");
+
+        String[] array = s.split("");
+
+        System.out.println(array[i-1]);
+
+    }
+}
+
