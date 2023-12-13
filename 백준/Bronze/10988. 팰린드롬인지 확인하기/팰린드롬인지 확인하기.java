5bf3dafe80f3d3274d2511cfbf05ddd42e57d56c
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-13 23:03:55 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 200 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -11,12 +11,16 @@ public class Main {
         int len = s.length();
         int ans = 1;
 
+        
+//        StringBuilder 의 reverse 사용
         StringBuilder sb = new StringBuilder(s);
-        sb.reverse();
+        sb.reverse(); 
 
-
+        // 문자열 길이 / 2만 하는 이유? : 검증 이정도만하면 되기 때문
+        // level 일경우 lev 까지 확인함
         for (int i = 0; i < len / 2; i++) {
-//            System.out.println(s.charAt(i));
+            // 확인차 출력해봄
+            // System.out.println(s.charAt(i));
             if(s.charAt(i) != sb.charAt(i)){
                 ans = 0;
             }
