99d18c0a879ad1b2288f14234a941101b294bab2
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-17 18:42:53 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 620 ms, Memory: 23960 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = N; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*N-((i+1)*2-1); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
+
+
+
