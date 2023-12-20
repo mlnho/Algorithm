dad671d2666304f3a0955b49e734d8b300d440b4
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-20 23:32:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String s) {
+
+        String [] numbers = {"zero","one","two","three","four",
+                             "five","six","seven","eight","nine"};
+        
+        for(int i=0; i<10; i++){
+                s = s.replace(numbers[i],Integer.toString(i));
+        }
+        
+        int answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
