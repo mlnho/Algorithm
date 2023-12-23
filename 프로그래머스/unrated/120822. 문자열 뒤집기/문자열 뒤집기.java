4f2e7d33113375557ff05b3357dcac1fba7af6be
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-23 16:20:10 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,15 @@
+import java.util.*;
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
-        for(int i = my_string.length(); i>0; i--){
-            answer += my_string.charAt(i-1);
-        }
+        sb.append(my_string);
         
-        return answer;
+        sb.reverse();
+        
+        return sb.toString();
+        
+        
     }
 }
