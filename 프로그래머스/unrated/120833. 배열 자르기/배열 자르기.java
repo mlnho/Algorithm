c45ed0606080bf70aea45da921faf5ff23baf95b
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-12-26 22:10:57 +0900

    [level unrated] Title: 배열 자르기, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        
+        answer = new int[num2-num1+1];
+        
+        int count = 0;
+        for(int i=num1; i<=num2; i++){
+            answer[count] = numbers[i];
+            count++;
+        }
+        
+        
+        return answer;
+    }
+}
