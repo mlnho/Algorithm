5fbfa604a11083b776ade8e09f99512aafc5c1b0
jin min ho <105831006+mlnho@users.noreply.github.com>
2024-01-15 19:04:46 +0900

    [Silver IV] Title: 스택 2, Time: 1076 ms, Memory: 237148 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
@@ -19,29 +20,18 @@ public class Main {
             if (tk.equals("1")){
                 stack.push(Integer.parseInt(st.nextToken()));
             } else if(tk.equals("2")){
-                if(stack.isEmpty()){
-                    sb.append("-1").append('\n');
-                } else {
-                    sb.append(stack.pop()).append('\n');
-                }
+                sb.append(stack.isEmpty() ? "-1" : stack.pop()).append('\n');
             } else if(tk.equals("3")){
                 sb.append(stack.size()).append('\n');
             } else if(tk.equals("4")){
-                if(!stack.isEmpty()){
-                    sb.append("0").append('\n');
-                } else {
-                    sb.append("1").append('\n');
-                }
+                sb.append(stack.isEmpty() ? "1" : "0").append('\n');
             } else if(tk.equals("5")){
-                if(stack.isEmpty()){
-                    sb.append("-1").append('\n');
-                } else {
-                    sb.append(stack.peek()).append('\n');
-                }
-
+                sb.append(stack.isEmpty() ? "-1" : stack.peek()).append('\n');
             }
         }
         System.out.println(sb.toString());
+
+        br.close();
     }
 }
 
