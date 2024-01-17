8318ebc0a539e6f9d6604554e822b90c54186031
jin min ho <105831006+mlnho@users.noreply.github.com>
2024-01-17 17:07:11 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+       
+        int[] answer = new int[commands.length];
+
+		for (int j = 0; j < commands.length; j++) {
+			int start = commands[j][0] - 1;
+			int end = commands[j][1] - 1;
+			int needNum = commands[j][2] - 1; // 새로운 배열에서 찾을 것
+
+			// System.out.println("needNum :" + needNum);
+
+			int a = array[start]; // array의 5
+			int b = array[end]; // array의 3
+			// System.out.println("a :" + a);
+			// System.out.println("b :" + b);
+
+			// 새로운 배열
+			int[] newArray = new int[commands[j][1] - commands[j][0] + 1];
+			int count = 0;
+			for (int k = 0; k < newArray.length; k++) {
+				newArray[k] = array[start + count];
+				count++;
+			}
+			Arrays.sort(newArray); // 새로운 배열 정렬
+			// System.out.println(Arrays.toString(newArray));
+
+			answer[j] = newArray[needNum];
+			// System.out.println(Arrays.toString(answer));
+		}
+        return answer;
+    }
+}
