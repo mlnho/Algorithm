3137653dde0f7f59fdc5e25af2bf397d9b0e641e
jin min ho <105831006+mlnho@users.noreply.github.com>
2024-01-17 11:48:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.97 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        	Stack<Integer> stack = new Stack<Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if(stack.isEmpty()) {
+				stack.push(arr[i]);
+			} else {
+				if(stack.peek()==arr[i]) {
+					continue;
+				} else {
+					stack.push(arr[i]);
+				}
+			}
+
+		}
+        
+        
+        answer = new int [stack.size()];
+		
+		for(int i=answer.length-1; i>=0; i--) {
+			answer[i] = stack.pop();
+		}
+        
+
+        return answer;
+    }
+}
