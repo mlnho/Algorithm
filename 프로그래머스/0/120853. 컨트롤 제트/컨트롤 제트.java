43eedf67bd31dda7680fc05fe9c4139a2862e0cb
jin min ho <105831006+mlnho@users.noreply.github.com>
2024-01-23 23:08:31 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.12 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -4,15 +4,9 @@ class Solution {
         
         String [] arr = s.split(" ");
         for(int i=0; i<arr.length; i++){
-            if(arr[i].equals("Z")){
-                arr[i]="0";
-                arr[i-1]="0";
-            }
-        }
-
-        for(int i=0; i<arr.length; i++){
             answer += Integer.parseInt(arr[i]);
         }
+        
         
         return answer;
     }
