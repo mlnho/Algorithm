9fb95d8b0565dff228002bd44396b0cdfb7cf700
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-04-11 23:09:07 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int changeInt = Integer.parseInt(n_str);
+        
+        answer = String.valueOf(changeInt);
+        
+        return answer;
+    }
+}
